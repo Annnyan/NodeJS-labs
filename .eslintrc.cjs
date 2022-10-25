b2362124d5c606b9c7345b8bcf95800b0f8a3c2e
Annnyan <novikovaannna@gmail.com>
2022-10-25 20:36:39 +0300

    add sonar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,13 @@ module.exports = {
     node: true,
     es2021: true
   },
-  extends: 'standard',
-  overrides: [
-  ],
+  extends: ['standard', 'plugin:sonarjs/recommended'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  'plugins': ['sonarjs'],
   rules: {
   }
 }
